3d9e96003817c8dfcac8cb7889c664cf709ec045
Devin Sherry <DevinSherry16@gmail.com>
2020-04-02 15:44:50 +0200

    Add files via upload

---
+++ Chapter14/Activity 14.02/ProjectileEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "ProjectileEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERSIDESCROLLER_API AProjectileEnemy : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
