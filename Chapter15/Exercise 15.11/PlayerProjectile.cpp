e443fc0dc1db085071c23d27b0d340c1366e06f8
Devin Sherry <DevinSherry16@gmail.com>
2020-05-02 19:57:02 +0200

    Add files via upload

---
+++ Chapter15/Exercise 15.11/PlayerProjectile.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "EnemyBase.h"
+
+// Sets default values
+APlayerProjectile::APlayerProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	// Use a sphere as a simple collision representation
+}
+
+void APlayerProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+}
+
+
