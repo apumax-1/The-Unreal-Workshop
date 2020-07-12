b1791f470eab8c40a919b4576a71bfdfb9cc484b
Devin Sherry <DevinSherry16@gmail.com>
2020-07-12 17:51:48 +0200

    Update PickableActor_Base.cpp

--- Chapter17/Exercise 17.01/PickableActor_Base.cpp
+++ Chapter17/Exercise 17.01/PickableActor_Base.cpp
@@ -5,8 +5,6 @@
 #include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/RotatingMovementComponent.h"
-#include "Kismet/GameplayStatics.h"
-#include "SuperSideScroller_Player.h"
 
 // Sets default values
 APickableActor_Base::APickableActor_Base()
@@ -14,7 +12,6 @@ APickableActor_Base::APickableActor_Base()
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
 	CollisionComp->InitSphereRadius(30.0f);
 	CollisionComp->BodyInstance.SetCollisionProfileName("OverlapAllDynamic");
-	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &APickableActor_Base::BeginOverlap);
 
 	// Set as root component
 	RootComponent = CollisionComp;
@@ -30,27 +27,4 @@ APickableActor_Base::APickableActor_Base()
 void APickableActor_Base::BeginPlay()
 {
 	Super::BeginPlay();
-}
-
-void APickableActor_Base::PlayerPickedUp(class ASuperSideScroller_Player* Player)
-{
-	UWorld* World = GetWorld();
-	if (World)
-	{
-		if (PickupSound)
-		{
-			UGameplayStatics::SpawnSoundAtLocation(World, PickupSound, GetActorLocation());
-		}
-	}
-
-	Destroy();
-}
-
-void APickableActor_Base::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	ASuperSideScroller_Player* Player = Cast<ASuperSideScroller_Player>(OtherActor);
-	if (Player)
-	{
-		PlayerPickedUp(Player);
-	}
 }
