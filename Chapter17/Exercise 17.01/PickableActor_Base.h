f94999a29ddde2f10c691a3c1f28baf085e4e904
Devin Sherry <DevinSherry16@gmail.com>
2020-07-12 17:52:20 +0200

    Update PickableActor_Base.h

--- Chapter17/Exercise 17.01/PickableActor_Base.h
+++ Chapter17/Exercise 17.01/PickableActor_Base.h
@@ -21,8 +21,6 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	virtual void PlayerPickedUp(class ASuperSideScroller_Player* Player);
-
 	/** Sphere collision component */
 	UPROPERTY(VisibleDefaultsOnly, Category = PickableItem)
 	class USphereComponent* CollisionComp;
@@ -33,14 +31,4 @@ protected:
 
 	UPROPERTY(VisibleDefaultsOnly, Category = PickableItem)
 	class URotatingMovementComponent* RotationComp;
-
-private:
-
-	UFUNCTION()
-	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-public:
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	class USoundBase* PickupSound;
 };
