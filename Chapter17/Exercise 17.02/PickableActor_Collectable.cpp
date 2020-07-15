0b42db9bf126634a8f772690e67ab7bd9aaeab79
Devin Sherry <DevinSherry16@gmail.com>
2020-07-15 19:56:13 +0200

    Add files via upload

---
+++ Chapter17/Exercise 17.02/PickableActor_Collectable.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickableActor_Collectable.h"
+//#include "SuperSideScroller_Player.h"
+//#include "Components/StaticMeshComponent.h"
+
+void APickableActor_Collectable::BeginPlay()
+{
+	Super::BeginPlay();
+	//MeshComp->SetPhysicsLinearVelocity(FVector(0.0f, 0.0f, 300.0f), false);
+}
+
+void APickableActor_Collectable::PlayerPickedUp(class ASuperSideScroller_Player* Player)
+{
+	//Player->IncrementNumberofCollectables(CollectableValue);
+	Super::PlayerPickedUp(Player);
+}
