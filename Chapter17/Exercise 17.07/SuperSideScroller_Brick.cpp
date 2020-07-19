e9dfc4c713ab5acfb0a3934f5361df2ed3a64d44
Devin Sherry <DevinSherry16@gmail.com>
2020-07-19 18:33:06 +0200

    Add files via upload

--- Chapter17/Exercise 17.07/SuperSideScroller_Brick.cpp
+++ Chapter17/Exercise 17.07/SuperSideScroller_Brick.cpp
@@ -12,7 +12,7 @@
 ASuperSideScroller_Brick::ASuperSideScroller_Brick()
 {
 	BrickMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BrickMesh"));
-	BrickMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BrickMesh->SetCollisionProfileName("BlockAll");
 
 	// Set as root component
 	RootComponent = BrickMesh;
@@ -27,8 +27,50 @@ ASuperSideScroller_Brick::ASuperSideScroller_Brick()
 void ASuperSideScroller_Brick::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
 
 void ASuperSideScroller_Brick::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
+	ASuperSideScroller_Player* Player = Cast<ASuperSideScroller_Player>(OtherActor);
+	if (Player && bHasCollectable)
+	{
+		AddCollectable(Player);
+		PlayHitSound();
+		PlayHitExplosion();
+		Destroy();
+	}
 }
+
+void ASuperSideScroller_Brick::AddCollectable(class ASuperSideScroller_Player* Player)
+{
+	if (bHasCollectable)
+	{
+		Player->IncrementNumberofCollectables(CollectableValue);
+	}
+}
+
+void ASuperSideScroller_Brick::PlayHitSound()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		if (HitSound)
+		{
+			UGameplayStatics::SpawnSoundAtLocation(World, HitSound, GetActorLocation());
+		}
+	}
+}
+
+void ASuperSideScroller_Brick::PlayHitExplosion()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		if (Explosion)
+		{
+			UGameplayStatics::SpawnEmitterAtLocation(World, Explosion, GetActorTransform());
+		}
+	}
+}
+
