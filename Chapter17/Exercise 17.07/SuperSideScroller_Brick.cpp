398b7604b048c364ad6ac793945d13a5bbdfdff6
Devin Sherry <DevinSherry16@gmail.com>
2020-07-19 18:33:47 +0200

    Update SuperSideScroller_Brick.cpp

--- Chapter17/Exercise 17.07/SuperSideScroller_Brick.cpp
+++ Chapter17/Exercise 17.07/SuperSideScroller_Brick.cpp
@@ -4,9 +4,6 @@
 #include "SuperSideScroller_Brick.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/BoxComponent.h"
-#include "Engine/World.h"
-#include "Kismet/GameplayStatics.h"
-#include "SuperSideScroller_Player.h"
 
 // Sets default values
 ASuperSideScroller_Brick::ASuperSideScroller_Brick()
@@ -27,50 +24,8 @@ ASuperSideScroller_Brick::ASuperSideScroller_Brick()
 void ASuperSideScroller_Brick::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void ASuperSideScroller_Brick::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	ASuperSideScroller_Player* Player = Cast<ASuperSideScroller_Player>(OtherActor);
-	if (Player && bHasCollectable)
-	{
-		AddCollectable(Player);
-		PlayHitSound();
-		PlayHitExplosion();
-		Destroy();
-	}
 }
-
-void ASuperSideScroller_Brick::AddCollectable(class ASuperSideScroller_Player* Player)
-{
-	if (bHasCollectable)
-	{
-		Player->IncrementNumberofCollectables(CollectableValue);
-	}
-}
-
-void ASuperSideScroller_Brick::PlayHitSound()
-{
-	UWorld* World = GetWorld();
-	if (World)
-	{
-		if (HitSound)
-		{
-			UGameplayStatics::SpawnSoundAtLocation(World, HitSound, GetActorLocation());
-		}
-	}
-}
-
-void ASuperSideScroller_Brick::PlayHitExplosion()
-{
-	UWorld* World = GetWorld();
-	if (World)
-	{
-		if (Explosion)
-		{
-			UGameplayStatics::SpawnEmitterAtLocation(World, Explosion, GetActorTransform());
-		}
-	}
-}
-
