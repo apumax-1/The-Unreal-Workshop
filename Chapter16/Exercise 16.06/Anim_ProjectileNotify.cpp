b5675c26d5c9a842cb867a71f7e6c52b0a857b9d
Devin Sherry <DevinSherry16@gmail.com>
2020-07-27 19:03:04 +0200

    Update Anim_ProjectileNotify.cpp

--- Chapter16/Exercise 16.06/Anim_ProjectileNotify.cpp
+++ Chapter16/Exercise 16.06/Anim_ProjectileNotify.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Anim_ProjectileNotify.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "SuperSideScroller/SuperSideScroller_Player.h"
 
 void UAnim_ProjectileNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
